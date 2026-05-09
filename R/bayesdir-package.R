b9235bbb7ef137fe51803b22c8ae94cfea409ed4
Sheng <jiangsheng08@gmail.com>
2026-05-09 15:26:29 +0800

    Initial commit

---
+++ R/bayesdir-package.R
@@ -0,0 +1,11 @@
+#' bayesdir
+#'
+#' This package implements the `bayesdir` method.
+#' 
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
