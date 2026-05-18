510c1d15b0db0a36942f5f3d93966a1168a06d9a
Xydd_0606 <2678586656@qq.com>
2026-05-18 21:49:38 +0800

    first rough package

--- bayesdir/R/all-helpers.R
+++ bayesdir/R/all-helpers.R
@@ -1,11 +1,11 @@
 # Generated from _main.Rmd: do not edit by hand
 
-
 #' Compute the function A(kappa) = I_{p/2}(kappa) / I_{p/2-1}(kappa)
 #' for the von Mises–Fisher distribution.
 #' @param kappa concentration parameter (non‑negative scalar)
 #' @param p space dimension (default = 2)
 #' @return scalar value of A(kappa)
+#' @export
 get_Ap <- function(kappa, p = 2) {
   if (kappa < 1e-6) return(0)
   besselI(kappa, p/2, expon.scaled = TRUE) /
@@ -17,6 +17,7 @@ get_Ap <- function(kappa, p = 2) {
 #' @param x unit vector observation (length p)
 #' @param p space dimension (default = 2)
 #' @return score vector of length p
+#' @export
 score_fn <- function(eta, x, p = 2) {
   kappa <- sqrt(sum(eta^2))
   if (kappa < 1e-6) return(x)
@@ -25,26 +26,39 @@ score_fn <- function(eta, x, p = 2) {
 }
 
 
-#' Draw a random unit vector from the vMF(eta) distribution on the circle. Used in simulation for comparison,
-#' Only implemented for p = 2.
-#' @param eta natural parameter vector (length 2)
-#' @return 2‑dimensional unit vector
-rvMF2 <- function(eta) {
-  k <- sqrt(sum(eta^2))
-  if (k < 1e-6) {
-    angle <- runif(1, 0, 2 * pi)
-  } else {
-    mu_angle <- atan2(eta[2], eta[1])
-    angle <- as.numeric(circular::rvonmises(1, circular::circular(mu_angle), k))
-  }
-  c(cos(angle), sin(angle))
-}
 
 #' Compute the sum of weights sum_{t = start}^{end} 1/(t + N)^2.
 #' @param start starting index (t >= 1)
 #' @param end ending index
 #' @param N number of observed data points
 #' @return scalar value of the sum
+#' @export
 compute_weight_sum <- function(start, end, N) {
   sum(1 / ((start:end) + N)^2)
 }
+
+#' Compute MLE given data X 
+#' @param X nrow = p ncol = number of observation
+#' @return MLE as a p-dim vector
+#' @export
+mle_vmf <- function(X){
+  n <- ncol(X)
+  
+  sum_vec <- rowSums(X)  # 2-dim vector sum
+  Robs <- sqrt(sum(sum_vec^2))
+  Rbar <- Robs / n
+  mu_hat <- atan2(sum_vec[2], sum_vec[1])
+  
+  if (Rbar < 0.001) {
+      kappa_hat <- 0
+    } else if (Rbar > 0.999) {
+      kappa_hat <- 1 / (2 * (1 - Rbar))
+    } else {
+      kappa_hat <- uniroot(function(k) get_Ap(k, 2) - Rbar, c(0.001, 100))$root
+    }
+  
+  c_hat <- kappa_hat * c(cos(mu_hat), sin(mu_hat))
+  
+  return(c_hat)
+}
+
---
+++ bayesdir/R/imports.R
@@ -0,0 +1,7 @@
+# Generated from _main.Rmd: do not edit by hand
+
+#' @importFrom parallel mclapply
+#' @importFrom stats quantile rnorm sd uniroot
+#' @importFrom rstiefel rmf.vector
+#' @importFrom magrittr %>%
+NULL
